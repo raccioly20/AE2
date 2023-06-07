0119e5efb442eeb3cc66c4c6bd2b8b017611eca0
raccioly20 <74776017+raccioly20@users.noreply.github.com>
2023-06-07 16:01:52 -0300

    AT++

--- sticker.R
+++ sticker.R
@@ -7,10 +7,10 @@ serie_img <- image_read("serie.png")
 sticker(
   subplot = serie_img,
   package = "AE2",
-  s_width = 0.8,
-  s_height = 0.8,
+  s_width = 1.3,
+  s_height = 1.3,
   s_x = 1,
-  s_y = 0.75,
+  s_y = 0.85,
   p_size = 10,
   p_color = "black",
   h_fill = "white"
